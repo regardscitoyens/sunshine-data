b508bc832425d64532b45eb999f5ec5b9e57bb2e
Tangui Morlier <tangui@tangui.eu.org>
2015-03-13 18:29:37 +0100

    unless rm

--- scripts/format_transparencesante_avantages.sh
+++ scripts/format_transparencesante_avantages.sh
@@ -6,39 +6,5 @@ sed -i 's/,30002530750,/,,/' data/tmp/transparencesante_avantages.formatted.csv
 sed -i 's/,12404201412,/,,/' data/tmp/transparencesante_avantages.formatted.csv
 sed -i 's/,MéA,/,Médecin,/' data/tmp/transparencesante_avantages.formatted.csv
 
-if false; then
-sed -i 1244931d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 1244930d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 1229175d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 1175885d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 1166379d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 1164878d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 1124104d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 1123388d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 1096793d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 1088304d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 1042911d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 1011486d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 1011485d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 931803d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 840758d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 792715d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 686503d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 686310d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 586418d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 582673d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 571969d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 517008d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 313855d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 313854d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 148597d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 141886d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 140971d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 140663d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 126197d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 107754d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 95347d data/tmp/transparencesante_avantages.formatted.csv
-sed -i 23407d data/tmp/transparencesante_avantages.formatted.csv
-fi
 mv data/tmp/transparencesante_avantages.formatted.csv data/formatted/transparencesante_avantages.formatted.csv
-rm data/tmp/transparencesante_avantages.formatted.csv
+
