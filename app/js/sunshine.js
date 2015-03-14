7a5fe8529beac28afd3af48695487819b7e2bceb
François Massot <francois.massot@gmail.com>
2015-03-14 14:52:50 +0100

    Load a given file and make doughnut with its data

--- app/js/sunshine.js
+++ app/js/sunshine.js
@@ -1,33 +1,152 @@
 //
 // Routines for sunshine landing page
 //
-(function(sunshine) {
+(function (sunshine) {
 
     sunshine.charts = {};
-    sunshine.data = {};
 
-    // Load data files
-    sunshine.load = function(url) {
-        Papa.parse(url, {
-            download: true,
-            complete: function(results) {
-                console.log("Csv " + url + " loaded : ", results);
-            }
-        })
+    sunshine.settings = {
+        montantAvantages: 'MONTANT AVANTAGES',
+        nbAvantages: 'NB AVANTAGES',
+        nbConventions: 'NB CONVENTIONS',
+        nbAvantagesConventions: 'NB TOTAL CONVENTIONS + AVANTAGES'
     };
 
-    sunshine.makeDoughnut = function(id, data) {
+    sunshine.makeDoughnut = function (id, data) {
+        console.log("Draw chart with data :", data);
         var ctx = document.getElementById(id).getContext("2d");
         sunshine.charts.labos = new Chart(ctx).Doughnut(data);
     };
 
+    sunshine.doughnut = function(id, data, value) {
+        var self = {
+            id: id,
+            data: data
+        };
+    };
 
-    // Load csv and make a chart
-    sunshine.test = function() {
-        console.log("Test loading of csv");
-        sunshine.load("/data/labos.departements.csv");
+    // Load data files
+    sunshine.load = function (name) {
+        return $.get("/data/" + name).then(function (data) {
+            var parsed = Papa.parse(data, {header: true});
+            console.log("Csv parsed", parsed);
+            return parsed;
+        });
+    };
+
+    sunshine.stats = function (data, dimensions) {
+        data = _.chain(data);
+
+        self = {};
+
+        function sumRows(left, right) {
+            var total = {};
+            _.forEach(sunshine.settings, function (value) {
+                total[value] = sunshine.utils.safeFloat(left[value]) + sunshine.utils.safeFloat(right[value]);
+            });
+            return total;
+        }
+
+        _.forEach(dimensions, function (dimension) {
+            self[dimension] = data
+                .groupBy(dimension)
+                .map(function (groupedDate, group) {
+                    var results = _.reduce(groupedDate, sumRows);
+                    results[dimension] = group;
+                    return results;
+                })
+                .sortBy(sunshine.settings.montantAvantages)
+                .reverse()
+                .value();
+        });
+
+        return self;
+    };
+
+
+    //
+    //
+    // Color scales
+    //
+    //
+    sunshine.scale = {};
+    sunshine.scale.LABO = function(name) {
+        colors = {
+            "NOVARTIS PHARMA": "#1f77b4",
+            SERVIER: "#aec7e8",
+            GLAXOSMITHKLINE: "#ff7f0e",
+            MSD: "#ffbb78",
+            "JANSSEN CILAG": "#2ca02c",
+            ASTRAZENECA: "#98df8a",
+            SANOFI: "#d62728",
+            PFIZER: "#ff9896",
+            "ASTELLAS PHARMA": "#9467bd",
+            "BOEHRINGER INGELHEIM": "#c5b0d5",
+            "DAIICHI SANKYO": "#8c564b",
+            LUNDBECK: "#c49c94",
+            ROCHE: "#e377c2",
+            GUERBET: "#f7b6d2",
+            BAYER: "#7f7f7f",
+            MEDTRONIC: "#c7c7c7",
+            "PIERRE FABRE MEDICAMENT": "#bcbd22",
+            "BIOGEN IDEC": "#dbdb8d",
+            "ST JUDE MEDICAL": "#17becf",
+            "ALK-Abello": "#9edae5"
+        };
+
+        if (_.isUndefined(colors[name])) {
+            return "#d9d9d9";
+        } else {
+            return colors[name];
+        }
+    };
+
+    //
+    //
+    // Utils routines
+    //
+    //
+    sunshine.utils = {};
+    sunshine.utils.safeFloat = function (string) {
+        if (_.isUndefined(string) || _.isEmpty(string)) {
+            return 0;
+        } else {
+            return +string;
+        }
+    };
+
+    //
+    //
+    // Tests
+    //
+    //
+    sunshine.test = function () {
         console.log("Test doughnut chart");
-        var data = [
+        sunshine.load("labos.departements.csv").done(function (response) {
+            var stats = sunshine.stats(response.data, ['LABO']);
+            var chartData = _(stats.LABO)
+                .slice(0, 20)
+                .map(function (labo) {
+                    return {
+                        value: labo[sunshine.settings.montantAvantages],
+                        color: sunshine.scale.LABO(labo),
+                        label: labo.LABO
+                    };
+                })
+                .value();
+            sunshine.makeDoughnut("labos", chartData);
+        });
+
+        //
+
+        var sampleData = [
+            {DEPARTEMENT: "32", LABO: "GAMBRO", "MONTANT AVANTAGES": "39", "NB AVANTAGES": "1", "NB CONVENTIONS": "", "NB TOTAL CONVENTIONS + AVANTAGES": "1"},
+            {DEPARTEMENT: "32", LABO: "GAMBRO", "MONTANT AVANTAGES": "10", "NB AVANTAGES": "1", "NB CONVENTIONS": "", "NB TOTAL CONVENTIONS + AVANTAGES": "1"},
+            {DEPARTEMENT: "33", LABO: "NOVARTIS", "MONTANT AVANTAGES": "39", "NB AVANTAGES": "1", "NB CONVENTIONS": "", "NB TOTAL CONVENTIONS + AVANTAGES": "1"},
+            {DEPARTEMENT: "34", LABO: "NOVARTIS", "MONTANT AVANTAGES": "39", "NB AVANTAGES": "1", "NB CONVENTIONS": "", "NB TOTAL CONVENTIONS + AVANTAGES": "1"},
+        ];
+
+        var chartSampleData = [
             {
                 value: 300,
                 color:"#F7464A",
@@ -46,8 +165,9 @@
                 highlight: "#FFC870",
                 label: "Yellow"
             }
-        ];
-        sunshine.makeDoughnut("labos", data);
+        ]
+
+        sunshine.makeDoughnut("test", chartSampleData);
     };
 
     $(document).ready(sunshine.test);
