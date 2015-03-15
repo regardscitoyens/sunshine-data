46e90b79d7bf9e2cf2258c761b161c6561e60de4
Tangui Morlier <tangui@tangui.eu.org>
2015-03-15 03:06:28 +0100

    Ne rentre pas dans les détails de la chirurgie

--- scripts/generate_public_beneficiaire_top.sh
+++ scripts/generate_public_beneficiaire_top.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo 'BENEFICIAIRE,DEPARTEMENT,NB TOTAL CONVENTIONS + AVANTAGES,NB CONVENTIONS,NB AVANTAGES,MONTANT AVANTAGES' > data/public/beneficiaires.top.csv
-sort -k 8,8 -t ',' -n -r data/public/beneficiaires.csv  | grep -v ',,' | head -n 5000 | awk -F ',' '{print $2" "$1" ("$3"),"$4","$5","$6","$7","$8}' | sed 's/ \(.....\)[^ ]* (/ \1 (/' >> data/public/beneficiaires.top.csv
+sort -k 8,8 -t ',' -n -r data/public/beneficiaires.csv  | grep -v ',,' | head -n 5000 | awk -F ',' '{print $2" "$1" ("$3"),"$4","$5","$6","$7","$8}' | sed 's/ \(.....\)[^ ]* (/ \1 (/' | sed 's/(Chirurgie[^)]*)/(Chirurgie)/' >> data/public/beneficiaires.top.csv
