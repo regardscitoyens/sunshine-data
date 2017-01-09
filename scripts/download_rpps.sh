9e04e29ee0d5c3b72aba3a14020c5787a6662c2e
Tangui Morlier <tangui@tangui.eu.org>
2017-01-09 17:00:41 +0100

    Gestion des changements de version du RPPS

--- scripts/download_rpps.sh
+++ scripts/download_rpps.sh
@@ -2,6 +2,7 @@
 
 url=$(curl -s -c /tmp/$$.jar --tlsv1.1 -k -L  https://annuaire.sante.fr/web/site-pro/extractions-publiques | sed 's|zip"><img src="/rass-pro-theme/images/bt_telecharger_espace_perso.png.*|zip|' | sed 's/.*href="//' | grep zip)
 curl -s -b /tmp/$$.jar --tlsv1.1 -k -L $url > /tmp/sunshine.zip
-unzip  /tmp/sunshine.zip -d /tmp/
-mv /tmp/ExtractionMonoTable_CAT18_ToutePopulation_201610040928.csv data/rpps.csv
-rm /tmp/sunshine.zip /tmp/$$.jar
+unzip  -B /tmp/sunshine.zip -d /tmp/ 2> /tmp/unzip.log 1>&2
+csv=$(grep inflating: /tmp/unzip.log | sed 's/ *inflating: *//' )
+mv $csv data/rpps.csv
+#rm /tmp/sunshine.zip /tmp/$$.jar /tmp/unzip.log
